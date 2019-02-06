6ad308bc10f5487c561ed6cfa385c37ac776c2b5
Bill Shoener <nitrocipher@gmail.com>
2019-02-05 23:30:02 -0600

    Upload block translation WIP

---
+++ blockdata.js
@@ -0,0 +1,395 @@
+{
+    motion_movesteps: {
+        blockcode: 'move %n steps'
+    },
+    motion_turnright: {
+        blockcode: 'turn cw %n degrees'
+    },
+    motion_turnleft: {
+        blockcode: 'turn ccw %n degrees'
+    },
+    motion_goto: {
+        blockcode: 'go to %m'
+    },
+    motion_goto_menu: {
+        blockcode: ''
+    },
+    motion_gotoxy: {
+        blockcode: 'go to x: %n y: %n'
+    },
+    motion_glideto: {
+        blockcode: 'glide %n secs to %m'
+    },
+    motion_glideto_menu: {
+        blockcode: ''
+    },
+    motion_glidesecstoxy: {
+        blockcode: 'glide %n secs to x: %n y: %n'
+    },
+    motion_pointindirection: {
+        blockcode: 'point in direction %n'
+    },
+    motion_pointtowards: {
+        blockcode: 'point towards %m'
+    },
+    motion_pointtowards_menu: {
+        blockcode: ''
+    },
+    motion_changexby: {
+        blockcode: 'change x by %n'
+    },
+    motion_setx: {
+        blockcode: 'set x to %n'
+    },
+    motion_changeyby: {
+        blockcode: 'change y by %n'
+    },
+    motion_sety: {
+        blockcode: 'set y to %n'
+    },
+    motion_ifonedgebounce: {
+        blockcode: 'if on edge, bounce'
+    },
+    motion_setrotationstyle: {
+        blockcode: 'set rotation style %m'
+    },
+    looks_sayforsecs: {
+        blockcode: 'say %s for %n seconds'
+    },
+    looks_say: {
+        blockcode: 'say %s'
+    },
+    looks_thinkforsecs: {
+        blockcode: 'think %s for %n seconds'
+    },
+    looks_think: {
+        blockcode: 'think %s'
+    },
+    looks_switchcostumeto: {
+        blockcode: 'switch costume to %m'
+    },
+    looks_costume: {
+        blockcode: ''
+    },
+    looks_nextcostume: {
+        blockcode: 'next costume'
+    },
+    looks_switchbackdropto: {
+        blockcode: 'switch back to %m'
+    },
+    looks_backdrops: {
+        blockcode: ''
+    },
+    looks_nextbackdrop: {
+        blockcode: 'next backdrop'
+    },
+    looks_changesizeby: {
+        blockcode: 'change size by %n'
+    },
+    looks_setsizeto: {
+        blockcode: 'set size to %n'
+    },
+    looks_changeeffectby: {
+        blockcode: 'change %m effect by %n'
+    },
+    looks_seteffectto: {
+        blockcode: 'set %m effect to %n'
+    },
+    looks_cleargraphiceffects: {
+        blockcode: 'clear graphics effects'
+    },
+    looks_show: {
+        blockcode: 'show'
+    },
+    looks_hide: {
+        blockcode: 'hide'
+    },
+    looks_gotofrontback: {
+        blockcode: 'go to %m layer'
+    },
+    looks_goforwardbackwardlayers: {
+        blockcode: 'go %m %n layers'
+    },
+    sound_playuntildone: {
+        blockcode: 'play %m until done'
+    },
+    sound_sounds_menu: {
+        blockcode: 'value'
+    },
+    sound_play: {
+        blockcode: 'value'
+    },
+    sound_sounds_menu: {
+        blockcode: 'value'
+    },
+    sound_stopallsounds: {
+        blockcode: 'value'
+    },
+    sound_changeeffectby: {
+        blockcode: 'value'
+    },
+    sound_seteffectto: {
+        blockcode: 'value'
+    },
+    sound_cleareffects: {
+        blockcode: 'value'
+    },
+    sound_changevolumeby: {
+        blockcode: 'value'
+    },
+    sound_setvolumeto: {
+        blockcode: 'value'
+    },
+    event_whenflagclicked: {
+        blockcode: 'value'
+    },
+    event_whenkeypressed: {
+        blockcode: 'value'
+    },
+    event_whenthisspriteclicked: {
+        blockcode: 'value'
+    },
+    event_whenbackdropswitchesto: {
+        blockcode: 'value'
+    },
+    event_whengreaterthan: {
+        blockcode: 'value'
+    },
+    event_whenbroadcastreceived: {
+        blockcode: 'value'
+    },
+    event_broadcast: {
+        blockcode: 'value'
+    },
+    event_broadcastandwait: {
+        blockcode: 'value'
+    },
+    control_wait: {
+        blockcode: 'value'
+    },
+    control_repeat: {
+        blockcode: 'value'
+    },
+    control_forever: {
+        blockcode: 'value'
+    },
+    control_if: {
+        blockcode: 'value'
+    },
+    control_if_else: {
+        blockcode: 'value'
+    },
+    control_wait_until: {
+        blockcode: 'value'
+    },
+    control_repeat_until: {
+        blockcode: 'value'
+    },
+    control_stop: {
+        blockcode: 'value'
+    },
+    control_start_as_clone: {
+        blockcode: 'value'
+    },
+    control_create_clone_of: {
+        blockcode: 'value'
+    },
+    control_create_clone_of_menu: {
+        blockcode: 'value'
+    },
+    control_delete_this_clone: {
+        blockcode: 'value'
+    },
+    motion_xposition: {
+        blockcode: 'value'
+    },
+    motion_yposition: {
+        blockcode: 'value'
+    },
+    motion_direction: {
+        blockcode: 'value'
+    },
+    looks_costumenumbername: {
+        blockcode: 'value'
+    },
+    looks_backdropnumbername: {
+        blockcode: 'value'
+    },
+    looks_size: {
+        blockcode: 'value'
+    },
+    sound_volume: {
+        blockcode: 'value'
+    },
+    sensing_touchingobject: {
+        blockcode: 'value'
+    },
+    sensing_touchingobjectmenu: {
+        blockcode: 'value'
+    },
+    sensing_touchingcolor: {
+        blockcode: 'value'
+    },
+    sensing_coloristouchingcolor: {
+        blockcode: 'value'
+    },
+    sensing_distanceto: {
+        blockcode: 'value'
+    },
+    sensing_distancetomenu: {
+        blockcode: 'value'
+    },
+    sensing_askandwait: {
+        blockcode: 'value'
+    },
+    sensing_answer: {
+        blockcode: 'value'
+    },
+    sensing_keypressed: {
+        blockcode: 'value'
+    },
+    sensing_keyoptions: {
+        blockcode: 'value'
+    },
+    sensing_mousedown: {
+        blockcode: 'value'
+    },
+    sensing_mousex: {
+        blockcode: 'value'
+    },
+    sensing_mousey: {
+        blockcode: 'value'
+    },
+    sensing_setdragmode: {
+        blockcode: 'value'
+    },
+    sensing_loudness: {
+        blockcode: 'value'
+    },
+    sensing_timer: {
+        blockcode: 'value'
+    },
+    sensing_resettimer: {
+        blockcode: 'value'
+    },
+    sensing_of: {
+        blockcode: 'value'
+    },
+    sensing_of_object_menu: {
+        blockcode: 'value'
+    },
+    sensing_current: {
+        blockcode: 'value'
+    },
+    sensing_dayssince2000: {
+        blockcode: 'value'
+    },
+    sensing_username: {
+        blockcode: 'value'
+    },
+    operator_add: {
+        blockcode: 'value'
+    },
+    operator_subtract: {
+        blockcode: 'value'
+    },
+    operator_multiply: {
+        blockcode: 'value'
+    },
+    operator_divide: {
+        blockcode: 'value'
+    },
+    operator_random: {
+        blockcode: 'value'
+    },
+    operator_gt: {
+        blockcode: 'value'
+    },
+    operator_lt: {
+        blockcode: 'value'
+    },
+    operator_equals: {
+        blockcode: 'value'
+    },
+    operator_and: {
+        blockcode: 'value'
+    },
+    operator_or: {
+        blockcode: 'value'
+    },
+    operator_not: {
+        blockcode: 'value'
+    },
+    operator_join: {
+        blockcode: 'value'
+    },
+    operator_letter_of: {
+        blockcode: 'value'
+    },
+    operator_length: {
+        blockcode: 'value'
+    },
+    operator_contains: {
+        blockcode: 'value'
+    },
+    operator_mod: {
+        blockcode: 'value'
+    },
+    operator_round: {
+        blockcode: 'value'
+    },
+    operator_mathop: {
+        blockcode: 'value'
+    },
+    undefined: {
+        blockcode: 'value'
+    },
+    data_setvariableto: {
+        blockcode: 'value'
+    },
+    data_changevariableby: {
+        blockcode: 'value'
+    },
+    data_showvariable: {
+        blockcode: 'value'
+    },
+    data_hidevariable: {
+        blockcode: 'value'
+    },
+    undefined: {
+        blockcode: 'value'
+    },
+    data_addtolist: {
+        blockcode: 'value'
+    },
+    data_deleteoflist: {
+        blockcode: 'value'
+    },
+    data_deletealloflist: {
+        blockcode: 'value'
+    },
+    data_insertatlist: {
+        blockcode: 'value'
+    },
+    data_replaceitemoflist: {
+        blockcode: 'value'
+    },
+    data_itemoflist: {
+        blockcode: 'value'
+    },
+    data_itemnumoflist: {
+        blockcode: 'value'
+    },
+    data_lengthoflist: {
+        blockcode: 'value'
+    },
+    data_listcontainsitem: {
+        blockcode: 'value'
+    },
+    data_showlist: {
+        blockcode: 'value'
+    },
+    data_hidelist: {
+        blockcode: 'value'
+    },
+}
