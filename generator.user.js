fa9c98a90f317ea703645e88e0ff880a0044efa3
Bill Shoener <nitrocipher@gmail.com>
2019-02-06 23:21:27 -0600

    Substacks! (block doubling happens, but im too tired to figure out why)

--- generator.user.js
+++ generator.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Blockify Scratch 3.0
 // @namespace    http://tampermonkey.net/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       NitroCipher
 // @match        https://scratch.mit.edu/blockify*
@@ -19,6 +19,7 @@
     var blockMapUrl = "https://raw.githubusercontent.com/s3blocks/s3bGenerator/master/blockdata.js"
     var derpyList = "";
     var blockMap;
+    var blockData;
     $.ajax({
         url: blockMapUrl,
         json: "text"
@@ -35,7 +36,7 @@
             sources: project.targets.map((stuff, index) => {
                 return {
                     name: stuff["name"],
-                    blocks: getBlocks(stuff["blocks"]),
+                    blocks: getAllBlocks(stuff["blocks"]),
                 }
             })
         }
@@ -46,63 +47,106 @@
         //$(".box-content").html("<pre>" + derpyList + "</pre>");
         //$(".box-content").html("<pre>" + js_beautify("{" + derpyList + "}") + "</pre>");
         $(".box-content").html("<pre>" + derpyList + "</pre>");
-        window.open("https://s3blocks.github.io/#" + encodeURI(derpyList))
+        //window.open("https://s3blocks.github.io/#" + encodeURI(derpyList))
     });
 
-    function getBlocks(blocks) {
+    function getAllBlocks(blocks) {
         document.pro = blocks;
-        var blockData = [];
-        for(var blockNum in blocks) {
+        blockData = [];
+        for(var blockID in blocks) {
             blockData.push({
-                opcode: blocks[blockNum].opcode,
-                parent: blocks[blockNum].parent
+                opcode: blocks[blockID].opcode,
+                parent: blocks[blockID].parent,
+                child: blocks[blockID].child
             });
+            //console.log(blockID);
+            var block = getBlock(blocks[blockID], blocks);
 
-            var thing = blocks[blockNum].opcode;
-            //alert(thing);
-            if (blockMap.hasOwnProperty(thing)) {
-                var blockCode = blockMap[thing].blockcode
-                blockCode = blockCode.split(" ");
-                var input = 0;
-                var field = 0;
-                blockCode.forEach(function(item, index) {
-                    switch (item) {
-                        default:
-                            blockCode[index] = blockCode[index];
-                            break;
-                        case "%n":
-                            blockCode[index] = "(" +blocks[blockNum].inputs[ Object.keys(blocks[blockNum].inputs)[input] ][1][1]+ ")";
-                            input++;
-                            break;
-                        case "%c":
-                            blockCode[index] = "[" +blocks[blockNum].inputs[ Object.keys(blocks[blockNum].inputs)[input] ][1][1]+ "]";
-                            input++;
-                            break;
-                        case "%s":
-                            blockCode[index] = "[" +blocks[blockNum].inputs[ Object.keys(blocks[blockNum].inputs)[input] ][1][1]+ "]";
-                            input++;
-                            break;
-                        case "%r":
-                            blockCode[index] = "(" +blocks[blockNum].inputs[ Object.keys(blocks[blockNum].inputs)[input] ][0]+ " v)";
-                            input++;
-                            break;
-                        case "%m":
-                            blockCode[index] = "[" +blocks[blockNum].fields[ Object.keys(blocks[blockNum].fields)[field] ][0]+ " v]";
-                            field++;
-                            break;
-                        case "%b":
-                            blockCode[index] = "<>";
-                            break;
-                        case "{}":
-                            blockCode[index] = "\nend";
-                            break;
-                    }
-
-                });
-                derpyList += blockCode.join(" ") + "\n";
+            if (blocks[blockID].parent === null) {
+                derpyList += "\n" + block + "\n";
+                getNextOf(blocks[blockID], blocks, 0);
+                //console.log(getNextOf(blocks[blockID], blocks, 1));
             }
         };
         return blockData;
+    }
+
+    function getNextOf(block, allBlocks, output, string) {
+        //console.log(JSON.stringify(block));
+        //console.log(allBlocks[block.next]);
+        if (block.next !== null) {
+            var blockChild = getBlock(allBlocks[block.next], allBlocks);
+            if (output == 1) {
+                return blockChild;
+            } else if (output == 2){
+                //console.log(substack);
+                //console.log(block);
+                var newString = string += "\n" + blockChild
+                console.log(newString)
+                return (getNextOf(allBlocks[block.next], allBlocks, output, newString));
+            } else {
+                //console.log(derpyList);
+                derpyList += blockChild + "\n";
+                getNextOf(allBlocks[block.next], allBlocks, output, string);
+            }
+        } else if (output == 2){
+            return string;
+        }
+    }
+
+    function getBlock(block, allBlocks) {
+        var thing = block.opcode;
+        //alert(thing);
+        if (blockMap.hasOwnProperty(thing)) {
+            var blockCode = blockMap[thing].blockcode
+            blockCode = blockCode.split(" ");
+            var input = 0;
+            var field = 0;
+            blockCode.forEach(function(item, index) {
+                var substack = "";
+                switch (item) {
+                    default:
+                        blockCode[index] = blockCode[index];
+                        break;
+                    case "%n":
+                        blockCode[index] = "(" +block.inputs[ Object.keys(block.inputs)[input] ][1][1]+ ")";
+                        input++;
+                        break;
+                    case "%c":
+                        blockCode[index] = "[" +block.inputs[ Object.keys(block.inputs)[input] ][1][1]+ "]";
+                        input++;
+                        break;
+                    case "%s":
+                        blockCode[index] = "[" +block.inputs[ Object.keys(block.inputs)[input] ][1][1]+ "]";
+                        input++;
+                        break;
+                    case "%r":
+                        blockCode[index] = "(" +block.inputs[ Object.keys(block.inputs)[input] ][0]+ " v)";
+                        input++;
+                        break;
+                    case "%m":
+                        blockCode[index] = "[" +block.fields[ Object.keys(block.fields)[field] ][0]+ " v]";
+                        field++;
+                        break;
+                    case "%b":
+                        blockCode[index] = "<>";
+                        input++
+                        break;
+                    case "{}":
+                        var subTop = block.inputs[ Object.keys(block.inputs)[input] ];
+                        substack += "\n" + getBlock(allBlocks[subTop[1]], allBlocks);
+                        substack += (getNextOf(allBlocks[subTop[1]], allBlocks, 2, substack));
+                        blockCode[index] = substack + "\nend";
+                        break;
+                }
+
+            });
+            //derpyList += blockCode.join(" ") + "\n";
+            //console.log(blockCode.join(" "));
+            return blockCode.join(" ");
+        } else {
+            return block.opcode;
+        }
     }
 
     function getUrlVars() {
